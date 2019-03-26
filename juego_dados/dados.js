bf5dfe2ca61c5c5d574aa9a6f3b777e40d1948cd
andres <andommartinez@gmail.com>
2019-03-26 17:16:37 +0100

    comprobar_condiciones

--- juego_dados/dados.js
+++ juego_dados/dados.js
@@ -1,34 +1,22 @@
 var dinero_jugador = 100;
 var dinero_maquina = 100;
 
+
+
 var veces_repetido = 0;
 
-var combinatoria_jugador;
+var combinatoria_jugador=-1;
 
 function tirarDado(){
 
-    var apuesta = document.getElementById("apuesta");
+    //var apuesta = document.getElementById("apuesta");
     var texto = document.getElementById("texto");
 
-    
+    var apuesta=2;
 
-    if(apuesta.value<dinero_jugador)
+    if(apuesta<dinero_jugador)
     {
-        var combinatoria =0;
-        /*
-        *
-        0. Nothing — five mismatched dice forming no sequence longer than four.
-        1. Pair — two dice showing the same value.
-        2. Two Pairs — two pairs of dice, each showing the same value.
-        3. Three-of-a-Kind — three dice showing the same value.
-        4. Five High Straight — dice showing values from 1 through 5, inclusive.
-        5. Six High Straight — dice showing values from 2 through 6, inclusive.
-        6. Full House — Pair of one value and Three-of-a-Kind of another.
-        7. Four-of-a-Kind — four dice showing the same value.
-        8. Five-of-a-Kind — all five dice showing the same value.
-        * 
-        * 
-        */        
+              
         var die1 = document.getElementById("die1");
         var die2 = document.getElementById("die2");
         var die3 = document.getElementById("die3");
@@ -42,41 +30,18 @@ function tirarDado(){
         var d4 = Math.floor(Math.random()*6)+1;
         var d5 = Math.floor(Math.random()*6)+1;
 
+        //Pasamos los valores a los dados html
+        die1.innerHTML=d1;
+        die2.innerHTML=d2;
+        die3.innerHTML=d3;
+        die4.innerHTML=d4;
+        die5.innerHTML=d5;
+
         //metemos dado en el array
 
         var valores_dados=[d1,d2,d3,d4,d5];
         var valores_dados_copia=[d1,d2,d3,d4,d5];
         var repeticiones = [];
-
-
-        for (i=0;i<valores_dados_copia.length;i++)
-        {
-            veces_repetido=1; //inicialmente ya contamos el valor
-            for(j=0;j<valores_dados_copia.length;j++)
-            {
-                if(valores_dados[i]==valores_dados_copia[j] && i!=j && valores_dados[i]!=0)
-                {
-                    veces_repetido++;
-                    valores_dados_copia[j]=0;
-                    console.log("El valor "+valores_dados_copia[i]+" esta: "+veces_repetido+ " veces repetido");
-                    repeticiones[i] = veces_repetido;
-                }            
-                
-            }
-            if(valores_dados_copia[i]!=0)
-                repeticiones[i] = veces_repetido;
-            else
-                repeticiones[i] = 0;
-        }
-
-        for (i=0;i<valores_dados.length;i++)
-        {
-            console.log("El valor "+valores_dados_copia[i]+" esta: "+repeticiones[i]+ " veces repetido");
-        }
-
-        
-        //Comprobar combinatorias
-
 
         /*
         *
@@ -94,6 +59,54 @@ function tirarDado(){
         * 
         */
 
+
+        for (i=0;i<valores_dados.length;i++)
+        {
+            veces_repetido=1; //inicialmente ya contamos el valor
+            for(j=0;j<valores_dados_copia.length;j++)
+            {
+                if(valores_dados[i]==valores_dados_copia[j] && i!=j && valores_dados[i]!=0)
+                {
+                    veces_repetido++;
+                    valores_dados[j]=0;
+                    //console.log("El valor "+valores_dados[i]+" esta: "+veces_repetido+ " veces repetido");
+                    repeticiones[i] = veces_repetido;
+                }            
+                
+            }
+            if(valores_dados[i]!=0)
+                repeticiones[i] = veces_repetido;
+            else
+                repeticiones[i] = 0;
+        }
+
+        for (i=0;i<valores_dados.length;i++)
+        {
+            console.log("El valor "+valores_dados[i]+" esta: "+repeticiones[i]+ " veces repetido");
+        }
+
+        
+        //Comprobar combinatorias
+
+        /*
+        *
+        0. Nothing — five mismatched dice forming no sequence longer than four.
+        1. Pair — two dice showing the same value.
+        2. Two Pairs — two pairs of dice, each showing the same value.
+        3. Three-of-a-Kind — three dice showing the same value.
+        4. Five High Straight — dice showing values from 1 through 5, inclusive.
+        5. Six High Straight — dice showing values from 2 through 6, inclusive.
+        6. Full House — Pair of one value and Three-of-a-Kind of another.
+        7. Four-of-a-Kind — four dice showing the same value.
+        8. Five-of-a-Kind — all five dice showing the same value.
+        * 
+        * 
+        */ 
+
+       var combinatoria =0;
+
+
+
         //Jugador
 
         diceTotal = d1+d2+d3+d4+d5;
@@ -101,32 +114,61 @@ function tirarDado(){
         if(repeticiones[0]==5)
         {
             combinatoria_jugador = 8; //8. Five-of-a-Kind — all five dice showing the same value.
+            status.innerHTML = "Repoker";
         }
         else if(repeticiones[0]==4 || repeticiones[1]==4)
         {
             combinatoria_jugador = 7; //7. Four-of-a-Kind — four dice showing the same value.
+            status.innerHTML = "Poker";
         }
         else if((repeticiones[0]==2 && repeticiones[1]==3) || (repeticiones[0]==3 && repeticiones[1]==2))
         {
             combinatoria_jugador = 6; //6. Full House — Pair of one value and Three-of-a-Kind of another.
+            status.innerHTML = "Full House";
         }
         else if(diceTotal==20 && (d1!=d2!=d3!=d4!=d5))
         {
             combinatoria_jugador = 5; //5. Six High Straight — dice showing values from 2 through 6, inclusive.
+            status.innerHTML = "Escalera de 6";
         }
-        else if(diceTotal==15 && (d1!=d2!=d3!=d4!=d5))
+        else if(diceTotal==15 && (d1!=d2 && d2!=d3 && d3!=d4 && d4!=d5))
         {
+            //!!!!!!!!!!!!!!!!!!!!!!!
+            //despues de comprobar si suma 5 comprobar con un for si todos son diferentes
+
             combinatoria_jugador = 4; //4. Five High Straight — dice showing values from 1 through 5, inclusive.
+            status.innerHTML = "Escalera";
         }
-        
+        else if(repeticiones [0]==3 || repeticiones [1]==3 || repeticiones [2]==3 || repeticiones [3]==3 || repeticiones [4]==3)
+        {
+            combinatoria_jugador = 3; //3. Three-of-a-Kind — three dice showing the same value.
+            status.innerHTML = "Tercia";
+        }
+        else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
+        {
+            var parejas=0;
+            for(i=0;i<repeticiones.length;i++)
+            {
+                if(repeticiones[i]==2)
+                    parejas++;
+            }
+            if(parejas==2)
+            {
+                combinatoria_jugador = 2; //2. Two Pairs — two pairs of dice, each showing the same value.
+                status.innerHTML = "Doble pareja";
+            }
+        }
+        else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
+        {
+            combinatoria_jugador = 1; //1. Pair — two dice showing the same value.
+            status.innerHTML = "Pareja";
+        }
+        else
+        {
+            combinatoria_jugador = 0; //0. Nothing — five mismatched dice forming no sequence longer than four.
+            status.innerHTML = "Nada";
+        }
   
-
-        status.innerHTML = "Has tirado un "+diceTotal+".";
-    
-        if(d1 == d2 ){
-            status.innerHTML = "DOBLES";
-        }
-
 
 
 
