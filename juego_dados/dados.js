fa887ba775dd081362e08b969992ec36d1cd454d
andommar <43680127+andommar@users.noreply.github.com>
2019-03-26 14:19:29 +0100

    dados casi final

--- juego_dados/dados.js
+++ juego_dados/dados.js
@@ -47,12 +47,6 @@ function tirarDado(){
         var valores_dados=[d1,d2,d3,d4,d5];
         var valores_dados_copia=[d1,d2,d3,d4,d5];
         var repeticiones = [];
-        //Comprobar combinatorias
-
-        if(d1==d2 && d2==d3 && d3==d4 && d4==d5)
-        {
-            combinatoria_jugador = 8; //8. Five-of-a-Kind — all five dice showing the same value.
-        }
 
 
         for (i=0;i<valores_dados_copia.length;i++)
@@ -69,43 +63,63 @@ function tirarDado(){
                 }            
                 
             }
-            valores_dados_copia[i]=0;
-            repeticiones[i] = veces_repetido;
+            if(valores_dados_copia[i]!=0)
+                repeticiones[i] = veces_repetido;
+            else
+                repeticiones[i] = 0;
         }
 
         for (i=0;i<valores_dados.length;i++)
         {
-            console.log("El valor "+valores_dados+" esta: "+veces_repetido[i]+ " veces repetido");
+            console.log("El valor "+valores_dados_copia[i]+" esta: "+repeticiones[i]+ " veces repetido");
         }
 
         
+        //Comprobar combinatorias
 
 
-       
         /*
-        
-        for(i=0;i<5;i++)
-        {
-            for(j=0;j<5;j++)
-            {
-                if(valores_dados[i]==valores_dados[j])
-                {
-                    
-                }
-            }
-            
-        }
+        *
+        * En el array valores_dados_copia se mantienen los valores de la tirada de dados. Cuando se encuentra un valor repetido, este se le da el valor de cero.
+        * En el array repeticiones se guardan las repeticiones de la tirada de dados.
+        * Ejemplo: Sale una tirada  de dados de 2 4 5 2 5.
+        * En la primera iteración se mirará cuantas veces se ha repetido el 2. Cuando encuentre que se ha repetido, ese valor pasará a ser 0 y se incremente veces_repetido.
+        * Es decir : 2 4 5 0 5, veces_repetido = 2.
+        * Para el 4, al no haber ningún repetido no variará el array. 2 4 5 0 5, veces_repetido = 1.
+        * Para el 5, 2 4 5 0 0, veces_repetido = 2.
+        * Cuando se llegue a los valores de 0, le asignamos al array de repeticiones un 0.
+        * Finalmente en el array de repeticiones se guardan las veces que un número ha salido repetido, en la misma posición de la tirada de dados.
+        * Array dados:          2 4 5 0 0
+        * Array repeticiones:   2 1 1 0 0   
+        * 
         */
 
+        //Jugador
 
+        diceTotal = d1+d2+d3+d4+d5;
 
-        var diceTotal = d1+d2+d3+d4+d5;
-        die1.innerHTML = d1;
-        die2.innerHTML = d2;
-        die3.innerHTML = d3;
-        die4.innerHTML = d4;
-        die5.innerHTML = d5;
-    
+        if(repeticiones[0]==5)
+        {
+            combinatoria_jugador = 8; //8. Five-of-a-Kind — all five dice showing the same value.
+        }
+        else if(repeticiones[0]==4 || repeticiones[1]==4)
+        {
+            combinatoria_jugador = 7; //7. Four-of-a-Kind — four dice showing the same value.
+        }
+        else if((repeticiones[0]==2 && repeticiones[1]==3) || (repeticiones[0]==3 && repeticiones[1]==2))
+        {
+            combinatoria_jugador = 6; //6. Full House — Pair of one value and Three-of-a-Kind of another.
+        }
+        else if(diceTotal==20 && (d1!=d2!=d3!=d4!=d5))
+        {
+            combinatoria_jugador = 5; //5. Six High Straight — dice showing values from 2 through 6, inclusive.
+        }
+        else if(diceTotal==15 && (d1!=d2!=d3!=d4!=d5))
+        {
+            combinatoria_jugador = 4; //4. Five High Straight — dice showing values from 1 through 5, inclusive.
+        }
+        
+  
 
         status.innerHTML = "Has tirado un "+diceTotal+".";
     
