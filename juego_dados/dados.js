ea0998fc460be08ad8ee59ab59495fec4c8f25db
andommar <43680127+andommar@users.noreply.github.com>
2019-03-27 13:54:30 +0100

    maqunia

--- juego_dados/dados.js
+++ juego_dados/dados.js
@@ -16,7 +16,7 @@ function tirarDado(){
 
     if(apuesta<dinero_jugador)
     {
-              
+        //jugador     
         var die1 = document.getElementById("die1");
         var die2 = document.getElementById("die2");
         var die3 = document.getElementById("die3");
@@ -30,18 +30,37 @@ function tirarDado(){
         var d4 = Math.floor(Math.random()*6)+1;
         var d5 = Math.floor(Math.random()*6)+1;
 
+        //maquina
+
+        var die6 = document.getElementById("die6");
+        var die7 = document.getElementById("die7");
+        var die8 = document.getElementById("die8");
+        var die9 = document.getElementById("die9");
+        var die10 = document.getElementById("die10");
+
+        var status2 = document.getElementById("status2");
+        var d6 = Math.floor(Math.random()*6)+1;
+        var d7 = Math.floor(Math.random()*6)+1;
+        var d8 = Math.floor(Math.random()*6)+1;
+        var d9 = Math.floor(Math.random()*6)+1;
+        var d10 = Math.floor(Math.random()*6)+1;
+
         //Pasamos los valores a los dados html
+        //Jugador
         die1.innerHTML=d1;
         die2.innerHTML=d2;
         die3.innerHTML=d3;
         die4.innerHTML=d4;
         die5.innerHTML=d5;
 
-        //metemos dado en el array
+        //maquina
+        die6.innerHTML=d6;
+        die7.innerHTML=d7;
+        die8.innerHTML=d8;
+        die9.innerHTML=d9;
+        die10.innerHTML=d10;
 
-        var valores_dados=[d1,d2,d3,d4,d5];
-        var valores_dados_copia=[d1,d2,d3,d4,d5];
-        var repeticiones = [];
+
 
         /*
         *
@@ -59,116 +78,182 @@ function tirarDado(){
         * 
         */
 
+        //metemos dado en el array
 
-        for (i=0;i<valores_dados.length;i++)
+        //1a vuelta jugador, 2a vuelta maquina
+
+        var valores_dados=[d1,d2,d3,d4,d5];
+        var valores_dados_copia=[d1,d2,d3,d4,d5];
+        var repeticiones = [];
+
+
+        for(i=0;i<2;i++)
         {
-            veces_repetido=1; //inicialmente ya contamos el valor
-            for(j=0;j<valores_dados_copia.length;j++)
+
+            var estado=status;
+
+            for (i=0;i<valores_dados.length;i++)
             {
-                if(valores_dados[i]==valores_dados_copia[j] && i!=j && valores_dados[i]!=0)
+                veces_repetido=1; //inicialmente ya contamos el valor
+                for(j=0;j<valores_dados_copia.length;j++)
                 {
-                    veces_repetido++;
-                    valores_dados[j]=0;
-                    //console.log("El valor "+valores_dados[i]+" esta: "+veces_repetido+ " veces repetido");
+                    if(valores_dados[i]==valores_dados_copia[j] && i!=j && valores_dados[i]!=0)
+                    {
+                        veces_repetido++;
+                        valores_dados[j]=0;
+                        repeticiones[i] = veces_repetido;
+                    }            
+                    
+                }
+                if(valores_dados[i]!=0)
                     repeticiones[i] = veces_repetido;
-                }            
-                
+                else
+                    repeticiones[i] = 0;
             }
-            if(valores_dados[i]!=0)
-                repeticiones[i] = veces_repetido;
+
+            for (i=0;i<valores_dados.length;i++)
+            {
+                console.log("El valor "+valores_dados[i]+" esta: "+repeticiones[i]+ " veces repetido");
+            }
+
+            
+            //Comprobar combinatorias
+
+            /*
+            *
+            0. Nothing — five mismatched dice forming no sequence longer than four.
+            1. Pair — two dice showing the same value.
+            2. Two Pairs — two pairs of dice, each showing the same value.
+            3. Three-of-a-Kind — three dice showing the same value.
+            4. Five High Straight — dice showing values from 1 through 5, inclusive.
+            5. Six High Straight — dice showing values from 2 through 6, inclusive.
+            6. Full House — Pair of one value and Three-of-a-Kind of another.
+            7. Four-of-a-Kind — four dice showing the same value.
+            8. Five-of-a-Kind — all five dice showing the same value.
+            * 
+            * 
+            */ 
+
+        var combinatoria =0;
+
+
+
+            //Jugador
+
+            diceTotal = valores_dados_copia[0]+valores_dados_copia[1]+valores_dados_copia[2]+valores_dados_copia[3]+valores_dados_copia[4];
+
+            if(repeticiones[0]==5)
+            {
+                combinatoria_jugador = 8; //8. Five-of-a-Kind — all five dice showing the same value.
+                estado.innerHTML = "Repoker";
+            }
+            else if(repeticiones[0]==4 || repeticiones[1]==4)
+            {
+                combinatoria_jugador = 7; //7. Four-of-a-Kind — four dice showing the same value.
+                estado.innerHTML = "Poker";
+            }
+            else if((repeticiones[0]==2 && repeticiones[1]==3) || (repeticiones[0]==3 && repeticiones[1]==2))
+            {
+                combinatoria_jugador = 6; //6. Full House — Pair of one value and Three-of-a-Kind of another.
+                estado.innerHTML = "Full House";
+            }
+            else if(diceTotal==20 )
+            {
+                var bandera = true;
+
+                for (i=0;i<valores_dados_copia.length;i++)
+                {
+                    for(j=0;j<valores_dados_copia.length;j++)
+                    {
+                        if(valores_dados_copia[i]==valores_dados_copia[j])
+                        {
+                            bandera=false;
+                        }
+                    }
+                }
+
+                if(bandera==true)
+                {
+                    combinatoria_jugador = 5; //5. Six High Straight — dice showing values from 2 through 6, inclusive.
+                    estado.innerHTML = "Escalera de 6";
+                }
+
+
+            }
+            else if(diceTotal==15)
+            {
+                var bandera = true;
+
+                for (i=0;i<valores_dados_copia.length;i++)
+                {
+                    for(j=0;j<valores_dados_copia.length;j++)
+                    {
+                        if(valores_dados_copia[i]==valores_dados_copia[j])
+                        {
+                            bandera=false;
+                        }
+                    }
+                }
+
+                if(bandera==true)
+                {
+                    combinatoria_jugador = 4; //4. Five High Straight — dice showing values from 1 through 5, inclusive.
+                    estado.innerHTML = "Escalera";
+                }
+
+
+            }
+            else if(repeticiones [0]==3 || repeticiones [1]==3 || repeticiones [2]==3 || repeticiones [3]==3 || repeticiones [4]==3)
+            {
+
+                combinatoria_jugador = 3; //3. Three-of-a-Kind — three dice showing the same value.
+                estado.innerHTML = "Tercia";
+            }
+            else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
+            {
+                var parejas=0;
+                for(i=0;i<repeticiones.length;i++)
+                {
+                    if(repeticiones[i]==2)
+                        parejas++;
+                }
+                if(parejas==2)
+                {
+                    combinatoria_jugador = 2; //2. Two Pairs — two pairs of dice, each showing the same value.
+                    estado.innerHTML = "Doble pareja";
+                }
+                else
+                {
+                    combinatoria_jugador = 1; //1. Pair — two dice showing the same value.
+                    estado.innerHTML = "Pareja";
+                }
+            }
             else
-                repeticiones[i] = 0;
-        }
+            {
 
-        for (i=0;i<valores_dados.length;i++)
-        {
-            console.log("El valor "+valores_dados[i]+" esta: "+repeticiones[i]+ " veces repetido");
-        }
+                combinatoria_jugador = 0; //0. Nothing — five mismatched dice forming no sequence longer than four.
+                estado.innerHTML = "Nada";
+            }
+            
 
+
+            for (i=0;i<valores_dados.length;i++)
+            {
+                console.log(repeticiones[i]+ " veces repetido");
+            }
+
+
+            //reemplazamos los dados del jugador por los de la maquina para hacer la 2a vuelta
+
+            var valores_dados=[d6,d7,d8,d9,d10];
+            var valores_dados_copia=[d6,d7,d8,d9,d10];
+     
+            estado = status2;
+
+
+        }
         
-        //Comprobar combinatorias
 
-        /*
-        *
-        0. Nothing — five mismatched dice forming no sequence longer than four.
-        1. Pair — two dice showing the same value.
-        2. Two Pairs — two pairs of dice, each showing the same value.
-        3. Three-of-a-Kind — three dice showing the same value.
-        4. Five High Straight — dice showing values from 1 through 5, inclusive.
-        5. Six High Straight — dice showing values from 2 through 6, inclusive.
-        6. Full House — Pair of one value and Three-of-a-Kind of another.
-        7. Four-of-a-Kind — four dice showing the same value.
-        8. Five-of-a-Kind — all five dice showing the same value.
-        * 
-        * 
-        */ 
-
-       var combinatoria =0;
-
-
-
-        //Jugador
-
-        diceTotal = d1+d2+d3+d4+d5;
-
-        if(repeticiones[0]==5)
-        {
-            combinatoria_jugador = 8; //8. Five-of-a-Kind — all five dice showing the same value.
-            status.innerHTML = "Repoker";
-        }
-        else if(repeticiones[0]==4 || repeticiones[1]==4)
-        {
-            combinatoria_jugador = 7; //7. Four-of-a-Kind — four dice showing the same value.
-            status.innerHTML = "Poker";
-        }
-        else if((repeticiones[0]==2 && repeticiones[1]==3) || (repeticiones[0]==3 && repeticiones[1]==2))
-        {
-            combinatoria_jugador = 6; //6. Full House — Pair of one value and Three-of-a-Kind of another.
-            status.innerHTML = "Full House";
-        }
-        else if(diceTotal==20 && (d1!=d2!=d3!=d4!=d5))
-        {
-            combinatoria_jugador = 5; //5. Six High Straight — dice showing values from 2 through 6, inclusive.
-            status.innerHTML = "Escalera de 6";
-        }
-        else if(diceTotal==15 && (d1!=d2 && d2!=d3 && d3!=d4 && d4!=d5))
-        {
-            //!!!!!!!!!!!!!!!!!!!!!!!
-            //despues de comprobar si suma 5 comprobar con un for si todos son diferentes
-
-            combinatoria_jugador = 4; //4. Five High Straight — dice showing values from 1 through 5, inclusive.
-            status.innerHTML = "Escalera";
-        }
-        else if(repeticiones [0]==3 || repeticiones [1]==3 || repeticiones [2]==3 || repeticiones [3]==3 || repeticiones [4]==3)
-        {
-            combinatoria_jugador = 3; //3. Three-of-a-Kind — three dice showing the same value.
-            status.innerHTML = "Tercia";
-        }
-        else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
-        {
-            var parejas=0;
-            for(i=0;i<repeticiones.length;i++)
-            {
-                if(repeticiones[i]==2)
-                    parejas++;
-            }
-            if(parejas==2)
-            {
-                combinatoria_jugador = 2; //2. Two Pairs — two pairs of dice, each showing the same value.
-                status.innerHTML = "Doble pareja";
-            }
-        }
-        else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
-        {
-            combinatoria_jugador = 1; //1. Pair — two dice showing the same value.
-            status.innerHTML = "Pareja";
-        }
-        else
-        {
-            combinatoria_jugador = 0; //0. Nothing — five mismatched dice forming no sequence longer than four.
-            status.innerHTML = "Nada";
-        }
-  
 
 
 
