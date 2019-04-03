5e8c70825cdc2a79f94f8248ecef0233fcb83af1
andommar <43680127+andommar@users.noreply.github.com>
2019-04-03 14:17:04 +0200

    dados_juego_final_v1

--- juego_dados/dados.js
+++ juego_dados/dados.js
@@ -6,6 +6,8 @@ var dinero_maquina = parseInt(100);
 var combinatoria_jugador=-1;
 var combinatoria=0;
 
+//dados guardados
+var dados_guardados = [0,0,0,0,0];
 
 // Valor bote
 var bote=parseInt(0);
@@ -24,16 +26,55 @@ dinero_j.innerHTML = dinero_jugador;
 dinero_m.innerHTML = dinero_maquina;
 bote_display.innerHTML = bote;
 
+//dados random
+
+//jugador     
+var die1 = document.getElementById("die1");
+var die2 = document.getElementById("die2");
+var die3 = document.getElementById("die3");
+var die4 = document.getElementById("die4");
+var die5 = document.getElementById("die5");
+
+var status1 = document.getElementById("status");
+
+
+
+var d1 = 0;
+var d2 = 0;
+var d3 = 0;
+var d4 = 0;
+var d5 = 0;
+
+
+
+
+
+//maquina
+
+var die6 = document.getElementById("die6");
+var die7 = document.getElementById("die7");
+var die8 = document.getElementById("die8");
+var die9 = document.getElementById("die9");
+var die10 = document.getElementById("die10");
+
+var status2 = document.getElementById("status2");
+
+var d6 = 0;
+var d7 = 0;
+var d8 = 0;
+var d9 = 0;
+var d10 = 0;
+
 
 //boton dados
 
-var b_dado1 = document.getElementById("b_dado1").disabled = true;
-var b_dado2 = document.getElementById("b_dado2").disabled = true;
-var b_dado3 = document.getElementById("b_dado3").disabled = true;
-var b_dado4 = document.getElementById("b_dado4").disabled = true;
-var b_dado5 = document.getElementById("b_dado5").disabled = true;
+document.getElementById("b_dado1").disabled = true;
+document.getElementById("b_dado2").disabled = true;
+document.getElementById("b_dado3").disabled = true;
+document.getElementById("b_dado4").disabled = true;
+document.getElementById("b_dado5").disabled = true;
 
-var b_continuar= document.getElementById("cont").disabled = true;
+document.getElementById("cont").disabled = true;
 
 
 
@@ -82,6 +123,7 @@ function Partida()
 
 function tirarDado(){
 
+    document.getElementById("tirar_dados").disabled = true;
 
     document.getElementById("b_dado1").disabled = false;
     document.getElementById("b_dado2").disabled = false;
@@ -93,41 +135,23 @@ function tirarDado(){
 
     var i,j,n,m;
 
+    d1 = Math.floor(Math.random()*6)+1;
+    d2 = Math.floor(Math.random()*6)+1;
+    d3 = Math.floor(Math.random()*6)+1;
+    d4 = Math.floor(Math.random()*6)+1;
+    d5 = Math.floor(Math.random()*6)+1;
+
+    d6 = Math.floor(Math.random()*6)+1;
+    d7 = Math.floor(Math.random()*6)+1;
+    d8 = Math.floor(Math.random()*6)+1;
+    d9 = Math.floor(Math.random()*6)+1;
+    d10 = Math.floor(Math.random()*6)+1;
 
 
     var veces_repetido = 0;
 
-    
-    var texto = document.getElementById("texto");
-        
-        //jugador     
-        var die1 = document.getElementById("die1");
-        var die2 = document.getElementById("die2");
-        var die3 = document.getElementById("die3");
-        var die4 = document.getElementById("die4");
-        var die5 = document.getElementById("die5");
+            
 
-        var status = document.getElementById("status");
-        var d1 = Math.floor(Math.random()*6)+1;
-        var d2 = Math.floor(Math.random()*6)+1;
-        var d3 = Math.floor(Math.random()*6)+1;
-        var d4 = Math.floor(Math.random()*6)+1;
-        var d5 = Math.floor(Math.random()*6)+1;
-
-        //maquina
-
-        var die6 = document.getElementById("die6");
-        var die7 = document.getElementById("die7");
-        var die8 = document.getElementById("die8");
-        var die9 = document.getElementById("die9");
-        var die10 = document.getElementById("die10");
-
-        var status2 = document.getElementById("status2");
-        var d6 = Math.floor(Math.random()*6)+1;
-        var d7 = Math.floor(Math.random()*6)+1;
-        var d8 = Math.floor(Math.random()*6)+1;
-        var d9 = Math.floor(Math.random()*6)+1;
-        var d10 = Math.floor(Math.random()*6)+1;
 
         //Pasamos los valores a los dados html
         //Jugador
@@ -169,8 +193,9 @@ function tirarDado(){
         var valores_dados=[d1,d2,d3,d4,d5];
         var valores_dados_copia=[d1,d2,d3,d4,d5];
         var repeticiones = [];
-        var estado=status;
+        var estado=status1;
         
+        console.log(estado);
 
 
         for(n=0;n<2;n++)
@@ -218,6 +243,8 @@ function tirarDado(){
             * 
             */ 
             diceTotal = valores_dados_copia[0]+valores_dados_copia[1]+valores_dados_copia[2]+valores_dados_copia[3]+valores_dados_copia[4];
+
+            console.log(estado);
 
             if(repeticiones[0]==5)
             {
@@ -333,9 +360,298 @@ function tirarDado(){
 
         }
         
-        this.ComprobarCombinatoria();
+        //this.ComprobarCombinatoria();
         
            
+}
+
+
+function DadoGuardado(id_boton)
+{
+
+    if(id_boton=="b_dado1")
+    {
+        dados_guardados[0]=d1;
+        document.getElementById("b_dado1").disabled = true;
+    }
+       
+    else if(id_boton=="b_dado2")
+    {
+        dados_guardados[1]=d2;
+        document.getElementById("b_dado2").disabled = true;
+    }
+       
+    else if(id_boton=="b_dado3")
+    {
+        dados_guardados[2]=d3;
+        document.getElementById("b_dado3").disabled = true;
+    }
+       
+    else if(id_boton=="b_dado4")
+    {
+        dados_guardados[3]=d4;
+        document.getElementById("b_dado4").disabled = true;
+    }
+       
+    else if(id_boton=="b_dado5")
+    {
+        dados_guardados[4]=d5;
+        document.getElementById("b_dado5").disabled = true;
+    }
+       
+
+       for(var m=0;m<dados_guardados.length;m++)
+       {
+           console.log("Posicion:"+[m]+" "+dados_guardados[m]);
+       }
+}
+
+
+function Continuar()
+{
+
+
+    document.getElementById("tirar_dados").disabled =false;
+
+    document.getElementById("b_dado1").disabled = true;
+    document.getElementById("b_dado2").disabled = true;
+    document.getElementById("b_dado3").disabled = true;
+    document.getElementById("b_dado4").disabled = true;
+    document.getElementById("b_dado5").disabled = true;
+    
+    document.getElementById("cont").disabled = true;
+    
+    var i,j,n,m;
+
+
+
+    var veces_repetido = 0;
+
+
+    d1 = Math.floor(Math.random()*6)+1;
+    d2 = Math.floor(Math.random()*6)+1;
+    d3 = Math.floor(Math.random()*6)+1;
+    d4 = Math.floor(Math.random()*6)+1;
+    d5 = Math.floor(Math.random()*6)+1;
+
+
+    d6 = Math.floor(Math.random()*6)+1;
+    d7 = Math.floor(Math.random()*6)+1;
+    d8 = Math.floor(Math.random()*6)+1;
+    d9 = Math.floor(Math.random()*6)+1;
+    d10 = Math.floor(Math.random()*6)+1;
+
+
+
+            //1a vuelta jugador, 2a vuelta maquina
+
+            var valores_dados=[d1,d2,d3,d4,d5];
+            var valores_dados_copia=[d1,d2,d3,d4,d5];
+
+            for (m=0;m<dados_guardados.length;m++)
+            {
+                if(dados_guardados[m]!=0){
+                    valores_dados[m]=dados_guardados[m];
+                    valores_dados_copia[m]=valores_dados[m];
+                }
+
+            }
+
+            for(var x=0;x<dados_guardados.length;x++)
+            {
+                console.log(valores_dados[x]);
+            }
+
+            var repeticiones = [];
+            var estado=status;
+            
+
+        //Pasamos los valores a los dados html
+        //Jugador
+        die1.innerHTML=valores_dados[0];
+        die2.innerHTML=valores_dados[1];
+        die3.innerHTML=valores_dados[2];
+        die4.innerHTML=valores_dados[3];
+        die5.innerHTML=valores_dados[4];
+
+        //maquina
+        die6.innerHTML=d6;
+        die7.innerHTML=d7;
+        die8.innerHTML=d8;
+        die9.innerHTML=d9;
+        die10.innerHTML=d10;
+    
+    
+            for(n=0;n<2;n++)
+            {
+    
+                for (i=0;i<valores_dados.length;i++)
+                {
+                    veces_repetido=1; //inicialmente ya contamos el valor
+                    for(j=0;j<valores_dados_copia.length;j++)
+                    {
+                        if(valores_dados[i]==valores_dados_copia[j] && i!=j && valores_dados[i]!=0)
+                        {
+                            veces_repetido++;
+                            valores_dados[j]=0;
+                            repeticiones[i] = veces_repetido;
+                        }            
+                        
+                    }
+                    if(valores_dados[i]!=0)
+                        repeticiones[i] = veces_repetido;
+                    else
+                        repeticiones[i] = 0;
+                }
+    
+                for (i=0;i<valores_dados.length;i++)
+                {
+                    console.log("El valor "+valores_dados[i]+" esta: "+repeticiones[i]+ " veces repetido");
+                }
+    
+                
+                //Comprobar combinatorias
+    
+                /*
+                *
+                0. Nothing — five mismatched dice forming no sequence longer than four.
+                1. Pair — two dice showing the same value.
+                2. Two Pairs — two pairs of dice, each showing the same value.
+                3. Three-of-a-Kind — three dice showing the same value.
+                4. Five High Straight — dice showing values from 1 through 5, inclusive.
+                5. Six High Straight — dice showing values from 2 through 6, inclusive.
+                6. Full House — Pair of one value and Three-of-a-Kind of another.
+                7. Four-of-a-Kind — four dice showing the same value.
+                8. Five-of-a-Kind — all five dice showing the same value.
+                * 
+                * 
+                */ 
+                diceTotal = valores_dados_copia[0]+valores_dados_copia[1]+valores_dados_copia[2]+valores_dados_copia[3]+valores_dados_copia[4];
+    
+                if(repeticiones[0]==5)
+                {
+                    combinatoria = 8; //8. Five-of-a-Kind — all five dice showing the same value.
+                    estado.innerHTML = "Repoker";
+                }
+                else if(repeticiones[0]==4 || repeticiones[1]==4)
+                {
+                    combinatoria = 7; //7. Four-of-a-Kind — four dice showing the same value.
+                    estado.innerHTML = "Poker";
+                }
+                else if((repeticiones[0]==2 && repeticiones[1]==3) || (repeticiones[0]==3 && repeticiones[1]==2))
+                {
+                    combinatoria = 6; //6. Full House — Pair of one value and Three-of-a-Kind of another.
+                    estado.innerHTML = "Full House";
+                }
+                else if(diceTotal==20)
+                {
+                    var bandera = true;
+    
+                    for (i=0;i<valores_dados_copia.length;i++)
+                    {
+                        for(j=0;j<valores_dados_copia.length;j++)
+                        {
+                            if(valores_dados_copia[i]==valores_dados_copia[j])
+                            {
+                                bandera=false;
+                            }
+                        }
+                    }
+    
+                    if(bandera==true)
+                    {
+                        combinatoria = 5; //5. Six High Straight — dice showing values from 2 through 6, inclusive.
+                        estado.innerHTML = "Escalera de 6";
+                    }
+    
+    
+                }
+                else if(diceTotal==15)
+                {
+                    var bandera = true;
+    
+                    for (i=0;i<valores_dados_copia.length;i++)
+                    {
+                        for(j=0;j<valores_dados_copia.length;j++)
+                        {
+                            if(valores_dados_copia[i]==valores_dados_copia[j])
+                            {
+                                bandera=false;
+                            }
+                        }
+                    }
+    
+                    if(bandera==true)
+                    {
+                        combinatoria = 4; //4. Five High Straight — dice showing values from 1 through 5, inclusive.
+                        estado.innerHTML = "Escalera";
+                    }
+    
+    
+                }
+                else if(repeticiones [0]==3 || repeticiones [1]==3 || repeticiones [2]==3 || repeticiones [3]==3 || repeticiones [4]==3)
+                {
+    
+                    combinatoria = 3; //3. Three-of-a-Kind — three dice showing the same value.
+                    estado.innerHTML = "Tercia";
+                }
+                else if(repeticiones [0]==2 || repeticiones [1]==2 || repeticiones [2]==2 || repeticiones [3]==2 || repeticiones [4]==2)
+                {
+                    var parejas=0;
+                    for(i=0;i<repeticiones.length;i++)
+                    {
+                        if(repeticiones[i]==2)
+                            parejas++;
+                    }
+                    if(parejas==2)
+                    {
+                        combinatoria = 2; //2. Two Pairs — two pairs of dice, each showing the same value.
+                        estado.innerHTML = "Doble pareja";
+                    }
+                    else
+                    {
+                        combinatoria = 1; //1. Pair — two dice showing the same value.
+                        estado.innerHTML = "Pareja";
+                    }
+                }
+                else
+                {
+                    combinatoria = 0; //0. Nothing — five mismatched dice forming no sequence longer than four.
+                    estado.innerHTML = "Nada";
+                }
+                
+    
+    
+                for (i=0;i<valores_dados.length;i++)
+                {
+                    console.log(repeticiones[i]+ " veces repetido");
+                }
+    
+    
+                //reemplazamos los dados del jugador por los de la maquina para hacer la 2a vuelta
+                var valores_dados=[d6,d7,d8,d9,d10];
+                var valores_dados_copia=[d6,d7,d8,d9,d10];
+                     
+                estado = status2;
+    
+                if(n==0)
+                    combinatoria_jugador= combinatoria;        
+                
+    
+    
+    
+            }
+            
+            this.ComprobarCombinatoria();
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -344,11 +660,11 @@ function MensajeAlerta(resultado){
 
     switch(resultado){
         case 0:
-        Swal.fire('Has perdido!');
+        swal("Has perdido la partida!");
         break;
 
         default:
-        Swal.fire('Has ganado!');
+        swal("Has ganado la partida!");
 
 
     }
@@ -361,7 +677,7 @@ function MensajeApuesta(){
 
 function ComprobarApuesta(valor_apuesta)
 {
-    if(valor_apuesta<dinero_jugador && valor_apuesta>0)
+    if(valor_apuesta<=dinero_jugador && valor_apuesta>0)
         return true;
     else
         return false;
@@ -422,9 +738,4 @@ function MensajeGanador(opcion){
     }
 } 
 
-
-function Dado1Guardado()
-{
-    document.getElementById("die1").value;
-}
 
